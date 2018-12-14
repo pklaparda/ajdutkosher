e7204a4d69861b4747e5ca083fd16850907087f7
danischermuk <daniel.schermuk@gmail.com>
2018-12-14 09:21:35 -0300

    Se agrega texto fijo sobre el Carousel. Se quitan los controles del carousel

--- client/src/components/empresas.js
+++ client/src/components/empresas.js
@@ -13,6 +13,8 @@ export default class Empresas extends Component {
 
 
     render() {
+        const controls = false;
+
         const items = [
             {
                 src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
@@ -36,7 +38,10 @@ export default class Empresas extends Component {
 
         return (
             <div>
-                <UncontrolledCarousel items={items} className="row" />
+                <UncontrolledCarousel controls={controls} items={items} className="row" />
+                <div style={{position: 'absolute', top: '100px', width: '96.666%', color: '#FFF', textAlign:'center'}}>
+                        <h2>Un Valor Agregado</h2>
+                </div>
                 <Row style={{ padding: '15px' }}>
                     <Col style={{ padding: '0' }}>
                         <img src={'/img/qek-super.jpg'} style={{ width: "100%", padding: '0' }} />
@@ -55,11 +60,17 @@ export default class Empresas extends Component {
                         style={{ width:'100%' }}
                     >
                         
-                        <div style={{ height:'200px', color:'white' }} >
-                            UNA VENTAJA COMPETITIVA
-                            <br/>
-                            DELCARACION DE CONFIDENCIALIDAD
-                        </div>
+                        <Row style={{ minHeight:'200px', color:'white' }} >
+                            <Col xs="12" sm="6" >
+                                UNA VENTAJA COMPETITIVA <br/>
+                                Garantiza al consumidor integridad, naturaleza y calidad. El sello Kosher no solo indica que la elaboración del producto se rige bajo las normas de la religión judía, sino también la estricta limpieza y saneamiento de los equipos de producción.
+
+                            </Col>
+                            <Col xs="12" sm="6">
+                                DECLARACIÓN DE CONFIDENCIALIDAD <br/>
+                                Cumplimos con los más estrictos niveles de confidencialidad. Somos extremadamente sensibles a las necesidades de la compañias y respetamos las necesidad de la información y secretos comerciales de los fabricantes.
+                            </Col>
+                        </Row>
                     </Parallax>
                 </Row>
                 <Row>
